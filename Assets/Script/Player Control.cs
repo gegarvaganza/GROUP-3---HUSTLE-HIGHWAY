a3757b5e92885cb6bc33b1c56d9a947d3b637617
gegarvaganza <ahmadhakim305@gmail.com>
2025-05-11 08:56:56 +0800

    Update 1
    Animation for run and turn right/left done

--- Assets/Script/Player Control.cs
+++ Assets/Script/Player Control.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Schema;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -15,10 +16,14 @@ public class PlayerControl : MonoBehaviour
     public bool SwipeRight;
     public float XValue;
     private CharacterController m_char;
+    private Animator m_Animator;
+    private float x;
+    public float SpeedDodge;
     void Start()
     {
         m_char = GetComponent<CharacterController>();
         transform.position = Vector3.zero;
+        m_Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,11 +37,13 @@ public class PlayerControl : MonoBehaviour
             {
                 NewXPos = -XValue;
                 m_SIDE = SIDE.Left;
+                m_Animator.Play("TurnLeft");
             }
             else if(m_SIDE == SIDE.Right)
             {
                 NewXPos = 0;
                 m_SIDE = SIDE.Mid;
+                m_Animator.Play("TurnLeft");
             }
         }
         else if (SwipeRight)
@@ -45,13 +52,16 @@ public class PlayerControl : MonoBehaviour
             {
                 NewXPos = XValue;
                 m_SIDE = SIDE.Right;
+                m_Animator.Play("TurnRight");
             }
             else if(m_SIDE == SIDE.Left)
             {
                 NewXPos = 0;
                 m_SIDE = SIDE.Mid;
+                m_Animator.Play("TurnRight");
             }
         }
-        m_char.Move((NewXPos - transform.position.x) * Vector3.right);
+        x = Mathf.Lerp(x,NewXPos,Time.deltaTime * SpeedDodge);
+        m_char.Move((x - transform.position.x) * Vector3.right);
     }
 }
