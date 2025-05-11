aecea13ef3b307f6976ba9ea1c913bbad57d3d37
gegarvaganza <ahmadhakim305@gmail.com>
2025-05-11 10:08:23 +0800

    Update 2

--- Assets/Script/Player Control.cs
+++ Assets/Script/Player Control.cs
@@ -12,13 +12,17 @@ public class PlayerControl : MonoBehaviour
 {
     public SIDE m_SIDE = SIDE.Mid; 
     float NewXPos = 0f;
-    public bool SwipeLeft;
-    public bool SwipeRight;
+    [HideInInspector]
+    public bool SwipeLeft, SwipeRight, SwipeUp;
     public float XValue;
     private CharacterController m_char;
     private Animator m_Animator;
     private float x;
     public float SpeedDodge;
+    public float JumpPower = 7f;
+    private float y;
+    public bool InJump;
+    public bool InRoll;
     void Start()
     {
         m_char = GetComponent<CharacterController>();
@@ -31,19 +35,21 @@ public class PlayerControl : MonoBehaviour
     {
         SwipeLeft = Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
         SwipeRight = Input.GetKeyDown (KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+        SwipeUp = Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+
         if (SwipeLeft)
         {
             if(m_SIDE == SIDE.Mid)
             {
                 NewXPos = -XValue;
                 m_SIDE = SIDE.Left;
-                m_Animator.Play("TurnLeft");
+                m_Animator.Play("Left");
             }
             else if(m_SIDE == SIDE.Right)
             {
                 NewXPos = 0;
                 m_SIDE = SIDE.Mid;
-                m_Animator.Play("TurnLeft");
+                m_Animator.Play("Left");
             }
         }
         else if (SwipeRight)
@@ -52,16 +58,42 @@ public class PlayerControl : MonoBehaviour
             {
                 NewXPos = XValue;
                 m_SIDE = SIDE.Right;
-                m_Animator.Play("TurnRight");
+                m_Animator.Play("Right");
             }
             else if(m_SIDE == SIDE.Left)
             {
                 NewXPos = 0;
                 m_SIDE = SIDE.Mid;
-                m_Animator.Play("TurnRight");
+                m_Animator.Play("Right");
             }
         }
+        Vector3 moveVector = new Vector3(x - transform.position.x, y * Time.deltaTime, 0);
         x = Mathf.Lerp(x,NewXPos,Time.deltaTime * SpeedDodge);
-        m_char.Move((x - transform.position.x) * Vector3.right);
+        m_char.Move(moveVector);
+        Jump();
+    }
+
+    public void Jump()
+    {
+        if(m_char.isGrounded)
+        {
+            if(m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Falling"))
+            {
+                m_Animator.Play("Landing");
+                InJump = false;
+            }
+            if (SwipeUp)
+            {
+                y = JumpPower;
+                m_Animator.CrossFadeInFixedTime("Jump", 0.1f);
+                InJump = true;
+            }
+            else
+            {
+                y -= JumpPower * 2 * Time.deltaTime;
+                if(m_char.velocity.y<-0.1f)
+                m_Animator.Play("Falling");
+            }
+        }
     }
 }
